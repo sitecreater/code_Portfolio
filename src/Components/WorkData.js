c6cce8b1285f603e0dbb179dd7a3d66ec6aa9bbc
park <pk8008@naver.com>
2022-09-14 15:36:49 +0900

    work Componentization

---
+++ src/Components/WorkData.js
@@ -0,0 +1,45 @@
+let data = [
+    {
+        id: 0,
+        image: "https://raw.githubusercontent.com/sitecreater/portfolioStorage.github.io/main/" +
+                "DrawaLogo.png",
+        title: "Drawa",
+        useLang: "사용 언어 : React, Firebase",
+        function: "주요 기능: react-query를 이용한 데이터 fetch, 검색기능 구현,framer-motion을 이용한 슬라이드, 모달창 등",
+        participation: "참여도 : 100%",
+        gitLink: "https://github.com/sitecreater/portfolioCode",
+        hostLink: "https://capstone-pwa.firebaseapp.com/"
+    }, {
+        id: 1,
+        image: "https://raw.githubusercontent.com/sitecreater/portfolioStorage.github.io/main/" +
+                "DrawaLogo.png",
+        title: "Java Project",
+        useLang: "사용 언어 : Java",
+        function: "주요 기능: ",
+        participation: "참여도 : 100%",
+        gitLink: "https://github.com/sitecreater/portfolioCode",
+        hostLink: "https://capstone-pwa.firebaseapp.com/"
+    }, {
+        id: 2,
+        image: "https://raw.githubusercontent.com/sitecreater/portfolioStorage.github.io/main/" +
+                "DrawaLogo.png",
+        title: "Clone Coding",
+        useLang: "사용 언어 : React",
+        function: "주요 기능: ",
+        participation: "참여도 : 100%",
+        gitLink: "https://github.com/sitecreater/portfolioCode",
+        hostLink: "https://capstone-pwa.firebaseapp.com/"
+    }, {
+        id: 3,
+        image: "https://raw.githubusercontent.com/sitecreater/portfolioStorage.github.io/main/" +
+                "DrawaLogo.png",
+        title: "Review Page",
+        useLang: "사용 언어 : Html, CSS, JS",
+        function: "주요 기능: 모달창",
+        participation: "참여도 : 100%",
+        gitLink: "https://github.com/sitecreater/portfolioCode",
+        hostLink: "https://capstone-pwa.firebaseapp.com/"
+    }
+]
+
+export default data;
